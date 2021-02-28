8119b2191b65330d3cee2274a5bca6f66564f8a5
aryanag7 <aryangarawal2310@gmail.com>
2021-03-01 00:01:17 +0530

    new

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'}
 
 url = "https://quotes.toscrape.com/"
-req = requests.get(url)
-soup= BeautifulSoup(req.text,'lxml')
+req = requests.get(url,headers=headers).text
+soup= BeautifulSoup(req,'lxml')
 
+Names=[]
+Prices=[]
 
-quote=soup.find_all('div', {'class': 'quote'})
+for i in soup.find_all('a',class_='a-link-normal a-text-normal'):
+    string=i.text
+    Names.append(i.strip())
 
-for i in quote:
-    spans = i.find('span', attrs={'class': 'text'})
-    author =i.find('small',attrs={'class':'author'})
-    for span in spans:
-        print(span.string,author.string)
-
+for each in Names:
+    print(each)
 
 
 
