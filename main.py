54638b424ac7b04996fac3713e07b33a13bd2536
aryanag7 <aryangarawal2310@gmail.com>
2021-03-01 15:02:50 +0530

    Laptop Web Scrapping

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'}
 
-url = "https://quotes.toscrape.com/"
+url = "https://www.amazon.in/s?k=laptops&ref=nb_sb_noss_2"
 req = requests.get(url,headers=headers).text
 soup= BeautifulSoup(req,'lxml')
 
@@ -12,11 +13,22 @@ Names=[]
 Prices=[]
 
 for i in soup.find_all('a',class_='a-link-normal a-text-normal'):
-    string=i.text
-    Names.append(i.strip())
+    string1=i.text
+    Names.append(string1.strip())
 
-for each in Names:
-    print(each)
+for i in soup.find_all('span',class_='a-price-whole'):
+    string2=i.text
+    Prices.append(string2)
+
+
+file_name = 'Laptops.csv'
+
+with open(file_name,'w') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Sr.no','Laptop Name','Prices'])
+
+    for i in range(len(Names)):
+        writer.writerow([i, Names[i], Prices[i]])
 
 
 
